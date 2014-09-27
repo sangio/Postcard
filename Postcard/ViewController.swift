4ea0c648e7fc5e4a5021adae43fc984ec7ed6cef
lsangiorgi90 <lsangiorgi90@gmail.com>
2014-09-27 17:39:00 +0200

    first rev of project

--- Postcard/ViewController.swift
+++ Postcard/ViewController.swift
@@ -10,16 +10,41 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    @IBOutlet weak var enterNameTextField: UITextField!
+    
+    @IBOutlet weak var enterMessageTextField: UITextField!
+    
+    var clicked = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        clicked = false
     }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
+    @IBAction func sendMailButtonPressed(sender: UIButton) {
+        // Code will evaluete when we press the button
+        clicked = !clicked
+        messageLabel.text = enterMessageTextField.text
+        enterMessageTextField.resignFirstResponder()
+        if clicked {
+            messageLabel.hidden=true
+        }
+        else {
+            messageLabel.hidden=false
+        }
+        
+        
+    }
 }
 
